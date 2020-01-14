6ed306fcaf7ec95e8142e57d29adbdbbdd3869d4
Siddhartha Anand <siddharthalibra13@gmail.com>
2020-01-14 22:49:04 +0530

    Update code with Player and Enemy Sprites

--- src/my_game.py
+++ src/my_game.py
@@ -1,17 +1,72 @@
+import random
 import pygame
 from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT,)
 
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surface = pygame.Surface((20, 20))
+        self.surface.fill((0, 255, 255))
+        self.rect = self.surface.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT)
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+# Defining a Player Sprite
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surface = pygame.Surface((75, 75))
+        # give the player/sprite a color
+        self.surface.fill((255, 255, 255))
+        # use this to draw a player later
+        self.rect = self.surface.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
 
 class Game(object):
-    def __init__(self, name=None, screen=None):
+    def __init__(self, name=None, screen=None, screen_width=500, screen_height=500):
         """Initialization"""
         self.name = name
         self.screen = screen
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.player = Player()
+        self.enemy = Enemy()
 
-    def create_window(self, screen_width=500, screen_height=500):
+    def create_window(self):
         """Create a sample window where the game will run."""
         pygame.init()
-        self.screen = pygame.display.set_mode([screen_width, screen_height])
+        self.screen = pygame.display.set_mode([self.screen_width, self.screen_height])
 
     def start_game_loop(self):
         # Run until the user asks to quit
@@ -25,11 +80,11 @@ class Game(object):
                         running = False
                 elif event.type == QUIT:
                     running = False
-            self.screen.fill([255, 255, 255])
-            surface = pygame.Surface((50, 50))
-            surface.fill((0, 0, 0))
-            rect = surface.get_rect()
-            self.screen.blit(surface, (400, 250))
+            pressed_keys = pygame.key.get_pressed()
+            self.player.update(pressed_keys)
+            self.screen.fill([0, 0, 0])
+            # Draw one surface on top of another
+            self.screen.blit(self.player.surface, self.player.rect)
             pygame.display.flip()
 
         pygame.quit()
@@ -41,6 +96,6 @@ class Game(object):
 
 
 if __name__ == '__main__':
-    window = Game('air-force-one')
-    window.create_window(800, 500)
+    window = Game('air-force-one', 500, 500)
+    window.create_window()
     window.start_game_loop()
